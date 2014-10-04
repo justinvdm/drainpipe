14cb69d5c576ba844e4462f616d39def5cadf7a8
justinvdm <justinvdm.0.1.0@gmail.com>
2014-10-04 17:31:03 +0200

    support umd

--- drainpipe.js
+++ drainpipe.js
@@ -1,27 +1,41 @@
-module.exports = drainpipe
-drainpipe.unwrap = unwrap
+;(function() {
+  drainpipe.unwrap = unwrap
 
 
-function drainpipe(x) {
-  var curr = unwrap(x)
-  self._drainpipe_chain = true
+  function drainpipe(x) {
+    var curr = unwrap(x)
+    self._drainpipe_chain = true
 
-  function self(fn) {
-    if (!arguments.length) return curr
+    function self(fn) {
+      if (!arguments.length) return curr
 
-    var args = Array.prototype.slice.call(arguments, 1)
-    args.unshift(curr)
-    curr = unwrap(fn.apply(fn, args))
+      var args = Array.prototype.slice.call(arguments, 1)
+      args.unshift(curr)
+      curr = unwrap(fn.apply(fn, args))
+
+      return self
+    }
 
     return self
   }
 
-  return self
-}
+
+  function unwrap(x) {
+    return (x || 0)._drainpipe_chain
+      ? x()
+      : x
+  }
 
 
-function unwrap(x) {
-  return (x || 0)._drainpipe_chain
-    ? x()
-    : x
-}
+  if (typeof module != 'undefined') {
+    module.exports = drainpipe
+  }
+  else if (typeof define == 'function' && define.amd) {
+    define(function() {
+      return drainpipe
+    })
+  }
+  else {
+    this.drainpipe = drainpipe
+  }
+}).call(this);
